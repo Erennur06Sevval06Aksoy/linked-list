5720370f07096262799ae61d1a6d4dbe13f06a0e
Şevval Aksoy <106920064+Erennur06Sevval06Aksoy@users.noreply.github.com>
2023-02-05 14:20:16 +0300

    Add files via upload

---
+++ bağlı liste ödev/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace bağlı_liste_ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEkleBastan = new System.Windows.Forms.Button();
+            this.buttonEkleSondan = new System.Windows.Forms.Button();
+            this.buttonEkleİstenilen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonEkleBastanDers = new System.Windows.Forms.Button();
+            this.buttonEkleSondanDers = new System.Windows.Forms.Button();
+            this.buttonEkleİstenilenDers = new System.Windows.Forms.Button();
+            this.buttonSilBastan = new System.Windows.Forms.Button();
+            this.buttonSilSondan = new System.Windows.Forms.Button();
+            this.buttonSilmeİstenilen = new System.Windows.Forms.Button();
+            this.buttonSilBastanOgr = new System.Windows.Forms.Button();
+            this.buttonSilSondanOgr = new System.Windows.Forms.Button();
+            this.buttonSilmeİstenilenOgr = new System.Windows.Forms.Button();
+            this.buttonSiralaNo = new System.Windows.Forms.Button();
+            this.buttonSiralaDers = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEkleBastan
+            // 
+            this.buttonEkleBastan.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleBastan.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleBastan.Location = new System.Drawing.Point(12, 12);
+            this.buttonEkleBastan.Name = "buttonEkleBastan";
+            this.buttonEkleBastan.Size = new System.Drawing.Size(121, 66);
+            this.buttonEkleBastan.TabIndex = 0;
+            this.buttonEkleBastan.Text = "DERS EKLEME (baştan)";
+            this.buttonEkleBastan.UseVisualStyleBackColor = false;
+            // 
+            // buttonEkleSondan
+            // 
+            this.buttonEkleSondan.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleSondan.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleSondan.Location = new System.Drawing.Point(139, 12);
+            this.buttonEkleSondan.Name = "buttonEkleSondan";
+            this.buttonEkleSondan.Size = new System.Drawing.Size(121, 66);
+            this.buttonEkleSondan.TabIndex = 1;
+            this.buttonEkleSondan.Text = "DERS EKLEME (sondan)";
+            this.buttonEkleSondan.UseVisualStyleBackColor = false;
+            // 
+            // buttonEkleİstenilen
+            // 
+            this.buttonEkleİstenilen.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleİstenilen.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleİstenilen.Location = new System.Drawing.Point(266, 12);
+            this.buttonEkleİstenilen.Name = "buttonEkleİstenilen";
+            this.buttonEkleİstenilen.Size = new System.Drawing.Size(121, 66);
+            this.buttonEkleİstenilen.TabIndex = 2;
+            this.buttonEkleİstenilen.Text = "DERS EKLEME (istenilen)";
+            this.buttonEkleİstenilen.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eklemek yada silmek istenilen sıra:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(300, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 28);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonEkleBastanDers
+            // 
+            this.buttonEkleBastanDers.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleBastanDers.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleBastanDers.Location = new System.Drawing.Point(12, 93);
+            this.buttonEkleBastanDers.Name = "buttonEkleBastanDers";
+            this.buttonEkleBastanDers.Size = new System.Drawing.Size(121, 76);
+            this.buttonEkleBastanDers.TabIndex = 5;
+            this.buttonEkleBastanDers.Text = "ÖĞRENCİ EKLEME (baştan)";
+            this.buttonEkleBastanDers.UseVisualStyleBackColor = false;
+            // 
+            // buttonEkleSondanDers
+            // 
+            this.buttonEkleSondanDers.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleSondanDers.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleSondanDers.Location = new System.Drawing.Point(139, 93);
+            this.buttonEkleSondanDers.Name = "buttonEkleSondanDers";
+            this.buttonEkleSondanDers.Size = new System.Drawing.Size(121, 76);
+            this.buttonEkleSondanDers.TabIndex = 6;
+            this.buttonEkleSondanDers.Text = "ÖĞRENCİ EKLEME (sondan)";
+            this.buttonEkleSondanDers.UseVisualStyleBackColor = false;
+            // 
+            // buttonEkleİstenilenDers
+            // 
+            this.buttonEkleİstenilenDers.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonEkleİstenilenDers.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonEkleİstenilenDers.Location = new System.Drawing.Point(266, 93);
+            this.buttonEkleİstenilenDers.Name = "buttonEkleİstenilenDers";
+            this.buttonEkleİstenilenDers.Size = new System.Drawing.Size(121, 76);
+            this.buttonEkleİstenilenDers.TabIndex = 7;
+            this.buttonEkleİstenilenDers.Text = "ÖĞRENCİ EKLEME (istenilen)";
+            this.buttonEkleİstenilenDers.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilBastan
+            // 
+            this.buttonSilBastan.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilBastan.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilBastan.Location = new System.Drawing.Point(12, 266);
+            this.buttonSilBastan.Name = "buttonSilBastan";
+            this.buttonSilBastan.Size = new System.Drawing.Size(121, 61);
+            this.buttonSilBastan.TabIndex = 8;
+            this.buttonSilBastan.Text = "DERS SİLME (baştan)";
+            this.buttonSilBastan.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilSondan
+            // 
+            this.buttonSilSondan.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilSondan.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilSondan.Location = new System.Drawing.Point(139, 266);
+            this.buttonSilSondan.Name = "buttonSilSondan";
+            this.buttonSilSondan.Size = new System.Drawing.Size(121, 61);
+            this.buttonSilSondan.TabIndex = 9;
+            this.buttonSilSondan.Text = "DERS SİLME (sondan)";
+            this.buttonSilSondan.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilmeİstenilen
+            // 
+            this.buttonSilmeİstenilen.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilmeİstenilen.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilmeİstenilen.Location = new System.Drawing.Point(266, 266);
+            this.buttonSilmeİstenilen.Name = "buttonSilmeİstenilen";
+            this.buttonSilmeİstenilen.Size = new System.Drawing.Size(121, 61);
+            this.buttonSilmeİstenilen.TabIndex = 10;
+            this.buttonSilmeİstenilen.Text = "DERS SİLME (istenilen)";
+            this.buttonSilmeİstenilen.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilBastanOgr
+            // 
+            this.buttonSilBastanOgr.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilBastanOgr.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilBastanOgr.Location = new System.Drawing.Point(12, 342);
+            this.buttonSilBastanOgr.Name = "buttonSilBastanOgr";
+            this.buttonSilBastanOgr.Size = new System.Drawing.Size(121, 68);
+            this.buttonSilBastanOgr.TabIndex = 11;
+            this.buttonSilBastanOgr.Text = "ÖĞRENCİ SİLME (baştan)";
+            this.buttonSilBastanOgr.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilSondanOgr
+            // 
+            this.buttonSilSondanOgr.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilSondanOgr.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilSondanOgr.Location = new System.Drawing.Point(139, 342);
+            this.buttonSilSondanOgr.Name = "buttonSilSondanOgr";
+            this.buttonSilSondanOgr.Size = new System.Drawing.Size(121, 68);
+            this.buttonSilSondanOgr.TabIndex = 12;
+            this.buttonSilSondanOgr.Text = "ÖĞRENCİ SİLME (sondan)";
+            this.buttonSilSondanOgr.UseVisualStyleBackColor = false;
+            // 
+            // buttonSilmeİstenilenOgr
+            // 
+            this.buttonSilmeİstenilenOgr.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSilmeİstenilenOgr.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSilmeİstenilenOgr.Location = new System.Drawing.Point(266, 342);
+            this.buttonSilmeİstenilenOgr.Name = "buttonSilmeİstenilenOgr";
+            this.buttonSilmeİstenilenOgr.Size = new System.Drawing.Size(121, 68);
+            this.buttonSilmeİstenilenOgr.TabIndex = 13;
+            this.buttonSilmeİstenilenOgr.Text = "ÖĞRENCİ SİLME (istenilen)";
+            this.buttonSilmeİstenilenOgr.UseVisualStyleBackColor = false;
+            // 
+            // buttonSiralaNo
+            // 
+            this.buttonSiralaNo.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSiralaNo.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSiralaNo.Location = new System.Drawing.Point(62, 421);
+            this.buttonSiralaNo.Name = "buttonSiralaNo";
+            this.buttonSiralaNo.Size = new System.Drawing.Size(121, 74);
+            this.buttonSiralaNo.TabIndex = 14;
+            this.buttonSiralaNo.Text = "NUMARAYA GÖRE SIRALA";
+            this.buttonSiralaNo.UseVisualStyleBackColor = false;
+            // 
+            // buttonSiralaDers
+            // 
+            this.buttonSiralaDers.BackColor = System.Drawing.Color.Aquamarine;
+            this.buttonSiralaDers.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSiralaDers.Location = new System.Drawing.Point(213, 421);
+            this.buttonSiralaDers.Name = "buttonSiralaDers";
+            this.buttonSiralaDers.Size = new System.Drawing.Size(121, 74);
+            this.buttonSiralaDers.TabIndex = 15;
+            this.buttonSiralaDers.Text = "DERS KODUNA GÖRE SIRALA";
+            this.buttonSiralaDers.UseVisualStyleBackColor = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(415, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 412);
+            this.listBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(300, 221);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 28);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Eklemek istenilen değer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(672, 508);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonSiralaDers);
+            this.Controls.Add(this.buttonSiralaNo);
+            this.Controls.Add(this.buttonSilmeİstenilenOgr);
+            this.Controls.Add(this.buttonSilSondanOgr);
+            this.Controls.Add(this.buttonSilBastanOgr);
+            this.Controls.Add(this.buttonSilmeİstenilen);
+            this.Controls.Add(this.buttonSilSondan);
+            this.Controls.Add(this.buttonSilBastan);
+            this.Controls.Add(this.buttonEkleİstenilenDers);
+            this.Controls.Add(this.buttonEkleSondanDers);
+            this.Controls.Add(this.buttonEkleBastanDers);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonEkleİstenilen);
+            this.Controls.Add(this.buttonEkleSondan);
+            this.Controls.Add(this.buttonEkleBastan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEkleBastan;
+        private System.Windows.Forms.Button buttonEkleSondan;
+        private System.Windows.Forms.Button buttonEkleİstenilen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonEkleBastanDers;
+        private System.Windows.Forms.Button buttonEkleSondanDers;
+        private System.Windows.Forms.Button buttonEkleİstenilenDers;
+        private System.Windows.Forms.Button buttonSilBastan;
+        private System.Windows.Forms.Button buttonSilSondan;
+        private System.Windows.Forms.Button buttonSilmeİstenilen;
+        private System.Windows.Forms.Button buttonSilBastanOgr;
+        private System.Windows.Forms.Button buttonSilSondanOgr;
+        private System.Windows.Forms.Button buttonSilmeİstenilenOgr;
+        private System.Windows.Forms.Button buttonSiralaNo;
+        private System.Windows.Forms.Button buttonSiralaDers;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
